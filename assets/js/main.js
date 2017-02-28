b9c8d0ebd62e00e8b4f874cd40c30dc9f8a77fc8
Sandeep Guduru <sandeep.gudur@gmail.com>
2017-03-01 02:26:53 +0530

    activity text removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "sandg");
+    GitHubCalendar("#github-graph", "sandheepg");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
