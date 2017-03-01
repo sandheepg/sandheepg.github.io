351ebe02544988eaa1c806a9ee17b21fd8039007
Sandeep Guduru <sandeep.gudur@gmail.com>
2017-03-01 21:39:44 +0530

    github activity limited and added link to musings

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ jQuery(document).ready(function($) {
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "sandheepg", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "sandheepg", selector: "#ghfeed", limit: 20 });
 
 
 });
